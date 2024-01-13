3d6c1bdb9764253d7f45d055d2542106bcad40d0
elaleksanyan <elizaa.aleksanyan@gmail.com>
2024-01-14 00:47:58 +0400

    button error

--- app/src/main/java/com/example/myapplicationnnnnnn/LearnFragmentActivity.java
+++ app/src/main/java/com/example/myapplicationnnnnnn/LearnFragmentActivity.java
@@ -5,20 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class LearnFragmentActivity extends AppCompatActivity {
 
-    private Button notes_btn, intervals_btn;
+    private Button notes_btn, intervals_btn, back_btn;
     private NotesFragment firstFragment = new NotesFragment();
     @Override    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_fragment);
         notes_btn = findViewById(R.id.notes);
         intervals_btn = findViewById(R.id.intervals);
+        back_btn = findViewById(R.id.button_back);
         setNewFragment();
+
         notes_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,7 +32,20 @@ public class LearnFragmentActivity extends AppCompatActivity {
             public void onClick(View v) {
                 IntervalsFragment secondFragment = new IntervalsFragment();
                 setNewFragment(secondFragment);            }
-        });    }
+        });
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LearnFragmentActivity.this, MainPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
 
     private void setNewFragment() {
     }
