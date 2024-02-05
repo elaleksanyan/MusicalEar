f8e7bba11c4983e1a47de8cba1ec236df8f0758c
elaleksanyan <elizaa.aleksanyan@gmail.com>
2024-02-05 20:31:35 +0400

    changes in activity_learn_page.xml and connected Firebase

--- app/src/main/java/com/example/myapplicationnnnnnn/LearnPageActivity.java
+++ app/src/main/java/com/example/myapplicationnnnnnn/LearnPageActivity.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 
 public class LearnPageActivity extends AppCompatActivity {
 
-    private Button notes_btn, intervals_btn, back_btn;
+    private Button notes_btn, intervals_btn;
     private NotesFragment firstFragment = new NotesFragment();
     private IntervalsFragment secondFragment = new IntervalsFragment();
     @Override    protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,36 +22,10 @@ public class LearnPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_learn_page);
         notes_btn = findViewById(R.id.notes);
         intervals_btn = findViewById(R.id.intervals);
-        back_btn = findViewById(R.id.button_back);
-        setNewFragment();
 
-        notes_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setNewFragment(firstFragment);            }
-        });
-        intervals_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IntervalsFragment secondFragment = new IntervalsFragment();
-                setNewFragment(secondFragment);            }
-        });
 
-        back_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LearnPageActivity.this, MainPageActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
-    private void setNewFragment() {}
-
-    private void setNewFragment(Fragment fragment) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame_layout, fragment);
-        ft.commit();    }
 
 
 
--- app/src/main/java/com/example/myapplicationnnnnnn/LoginActivity.java
+++ app/src/main/java/com/example/myapplicationnnnnnn/LoginActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText username;
