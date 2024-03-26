da90b6088da13b1e0e5e6b2dc4ab8dd5b441cc1a
elaleksanyan <elizaa.aleksanyan@gmail.com>
2024-03-27 03:07:45 +0400

    LearnNotes with FireBase

--- app/src/main/java/com/example/myapplicationnnnnnn/NotesLearn.java
+++ app/src/main/java/com/example/myapplicationnnnnnn/NotesLearn.java
@@ -1,5 +1,6 @@
 package com.example.myapplicationnnnnnn;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -65,4 +66,224 @@ public class NotesLearn extends AppCompatActivity {
 
 
     }
+    public void c_listen(View view) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference().child("NotesMp3/do.mp3");
+
+        try {
+            final MediaPlayer mediaPlayer = new MediaPlayer();
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    try {
+                        // Set the data source to the local file
+                        mediaPlayer.setDataSource(getApplicationContext(), uri);
+                        // Prepare the MediaPlayer
+                        mediaPlayer.prepare();
+                        // Start playing the audio
+                        mediaPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle any errors
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void d_listen(View view) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference().child("NotesMp3/re.mp3");
+
+        try {
+            final MediaPlayer mediaPlayer = new MediaPlayer();
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    try {
+                        // Set the data source to the local file
+                        mediaPlayer.setDataSource(getApplicationContext(), uri);
+                        // Prepare the MediaPlayer
+                        mediaPlayer.prepare();
+                        // Start playing the audio
+                        mediaPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle any errors
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void e_listen(View view) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference().child("NotesMp3/mi.mp3");
+
+        try {
+            final MediaPlayer mediaPlayer = new MediaPlayer();
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    try {
+                        // Set the data source to the local file
+                        mediaPlayer.setDataSource(getApplicationContext(), uri);
+                        // Prepare the MediaPlayer
+                        mediaPlayer.prepare();
+                        // Start playing the audio
+                        mediaPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle any errors
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void f_listen(View view) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference().child("NotesMp3/fa.mp3");
+
+        try {
+            final MediaPlayer mediaPlayer = new MediaPlayer();
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    try {
+                        // Set the data source to the local file
+                        mediaPlayer.setDataSource(getApplicationContext(), uri);
+                        // Prepare the MediaPlayer
+                        mediaPlayer.prepare();
+                        // Start playing the audio
+                        mediaPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle any errors
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void g_listen(View view) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference().child("NotesMp3/sol.mp3");
+
+        try {
+            final MediaPlayer mediaPlayer = new MediaPlayer();
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    try {
+                        // Set the data source to the local file
+                        mediaPlayer.setDataSource(getApplicationContext(), uri);
+                        // Prepare the MediaPlayer
+                        mediaPlayer.prepare();
+                        // Start playing the audio
+                        mediaPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle any errors
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void a_listen(View view) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference().child("NotesMp3/la.mp3");
+
+        try {
+            final MediaPlayer mediaPlayer = new MediaPlayer();
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    try {
+                        // Set the data source to the local file
+                        mediaPlayer.setDataSource(getApplicationContext(), uri);
+                        // Prepare the MediaPlayer
+                        mediaPlayer.prepare();
+                        // Start playing the audio
+                        mediaPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle any errors
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void h_listen(View view) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference().child("NotesMp3/si.mp3");
+
+        try {
+            final MediaPlayer mediaPlayer = new MediaPlayer();
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    try {
+                        // Set the data source to the local file
+                        mediaPlayer.setDataSource(getApplicationContext(), uri);
+                        // Prepare the MediaPlayer
+                        mediaPlayer.prepare();
+                        // Start playing the audio
+                        mediaPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle any errors
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
