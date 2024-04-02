ab6e067aec5f7a04094b2d93e1581f5333e4a12d
elaleksanyan <elizaa.aleksanyan@gmail.com>
2024-04-03 01:18:51 +0400

    IntervalsLearn + working listen button

--- app/src/main/java/com/example/myapplicationnnnnnn/IntervalsLearn.java
+++ app/src/main/java/com/example/myapplicationnnnnnn/IntervalsLearn.java
@@ -1,5 +1,6 @@
 package com.example.myapplicationnnnnnn;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,12 +28,15 @@ public class IntervalsLearn extends AppCompatActivity {
 
     ImageView onPiano,onNotes;
     TextView textView;
-    Button next_int;
-    StorageReference storageReference1,storageReference2;
+    Button next_int, listen;
+    StorageReference storageReference1,storageReference2,storageReference3;
     FirebaseFirestore db;
     List<String> names;
     List<String> images;
     List<String> sheets;
+    List<String> audioNames;
+
+    private MediaPlayer mediaPlayer;
     int currentIndex = 0;
 
     @Override
@@ -87,22 +92,63 @@ public class IntervalsLearn extends AppCompatActivity {
         sheets.add("mec_septima.png");
         sheets.add("oktava.png");
 
+        listen = findViewById(R.id.listen_interval);
+        storageReference3 = storage.getReference("Intervals/IntervalsMp3");
+
+        audioNames = new ArrayList<>();
+
+        audioNames.add("prima.mp3");
+        audioNames.add("poqr_sekunda.mp3");
+        audioNames.add("mec_sekunda.mp3");
+        audioNames.add("poqr_tercia.mp3");
+        audioNames.add("mec_tercia.mp3");
+        audioNames.add("maqur_kvarta.mp3");
+        audioNames.add("maqur_kvinta.mp3");
+        audioNames.add("poqr_seksta.mp3");
+        audioNames.add("mec_seksta.mp3");
+        audioNames.add("poqr_septima.mp3");
+        audioNames.add("mec_septima.mp3");
+        audioNames.add("oktava.mp3");
+
         // Set initial image and string
         loadNextImage();
         fetchNextStringFromFirestore();
+
+        mediaPlayer = new MediaPlayer();
+
+
+        listen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playNextAudio();
+            }
+        });
+
 
         next_int.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 currentIndex++;
                 if (currentIndex >= images.size()) {
-                    currentIndex = 0; // Reset index when reaching end of images
+                    currentIndex = 0;
                 }
                 loadNextImage();
                 fetchNextStringFromFirestore();
             }
+
         });
+
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
 
     private void fetchNextStringFromFirestore() {
         // Get document from Firestore collection
@@ -130,9 +176,48 @@ public class IntervalsLearn extends AppCompatActivity {
                 });
     }
 
+    private void playNextAudio() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        mediaPlayer = new MediaPlayer();
+
+        StorageReference audioFileRef = storageReference3.child(audioNames.get(currentIndex));
+        audioFileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                try {
+                    mediaPlayer.setDataSource(getApplicationContext(), uri);
+                    mediaPlayer.prepareAsync();
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mediaPlayer.start();
+                        }
+                    });
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            mediaPlayer.release();
+                        }
+                    });
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Failed to prepare audio", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failed to download audio", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void loadNextImage() {
         String imageName = images.get(currentIndex);
         String sheetsView = sheets.get(currentIndex);
+
         StorageReference imageRef = storageReference1.child(imageName);
         StorageReference sheetsRef = storageReference2.child(sheetsView);
 
@@ -163,6 +248,7 @@ public class IntervalsLearn extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Failed to load image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+
 
     }
 }
