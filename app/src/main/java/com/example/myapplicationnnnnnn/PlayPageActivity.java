3763946c2909eefddb5f1eedddea60507c3e5802
elaleksanyan <elizaa.aleksanyan@gmail.com>
2024-04-11 19:15:46 +0400

    quiz xml

--- app/src/main/java/com/example/myapplicationnnnnnn/PlayPageActivity.java
+++ app/src/main/java/com/example/myapplicationnnnnnn/PlayPageActivity.java
@@ -1,117 +1,22 @@
 package com.example.myapplicationnnnnnn;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
 
-
-import androidx.appcompat.app.AlertDialog;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+
+
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class PlayPageActivity extends AppCompatActivity {
 
 
-    TextView totalQuestionsTextView;
-    TextView questionTextView;
-    Button ansAButton;
-    Button ansBButton;
-    Button ansCButton;
-    Button ansDButton;
-    Button okButton;
-    String selectedAnswer = "";
-    int currentQuestionIndex = 0;
-    int score = 0;
-    int totalQuestions = QuestionAnswers.question.length;
-
-    Handler handler = new Handler();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_page);
-
-        FirebaseApp.initializeApp(this);
-
-
-
-        totalQuestionsTextView = findViewById(R.id.totalQuestions);
-        questionTextView = findViewById(R.id.question);
-        ansAButton = findViewById(R.id.ans_a);
-        ansBButton = findViewById(R.id.ans_b);
-        ansCButton = findViewById(R.id.ans_c);
-        ansDButton = findViewById(R.id.ans_d);
-        okButton = findViewById(R.id.ok);
-
-        loadNewQuestion();
-
-    }
-
-    public void onClick(View view) {
-        ansAButton.setBackgroundColor(Color.WHITE);
-        ansBButton.setBackgroundColor(Color.WHITE);
-        ansCButton.setBackgroundColor(Color.WHITE);
-        ansDButton.setBackgroundColor(Color.WHITE);
-
-        Button button = (Button) view;
-
-
-            selectedAnswer = button.getText().toString();
-            if (selectedAnswer.equals(QuestionAnswers.answers[currentQuestionIndex])) {
-                button.setBackgroundColor(Color.GREEN);
-                score++;
-            }else {
-                button.setBackgroundColor(Color.RED);
-            };
-            ++currentQuestionIndex;
-            loadNewQuestion();
-
-    }
-    private void loadNewQuestion() {
-        if (currentQuestionIndex == totalQuestions) {
-            okButton.setEnabled(false);
-            FinishQuiz();
-            return;
-        }
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                questionTextView.setText(QuestionAnswers.question[currentQuestionIndex]);
-                ansAButton.setText(QuestionAnswers.choices[currentQuestionIndex][0]);
-                ansBButton.setText(QuestionAnswers.choices[currentQuestionIndex][1]);
-                ansCButton.setText(QuestionAnswers.choices[currentQuestionIndex][2]);
-                ansDButton.setText(QuestionAnswers.choices[currentQuestionIndex][3]);
-            }
-        }, 2000);
-    }
-    private void FinishQuiz() {
-        String passStatus = "";
-        if (score > totalQuestions * 0.6) {
-            passStatus = "Passed";
-        } else {
-            passStatus = "Failed";
-        }
-
-        new AlertDialog.Builder(this)
-                .setTitle(passStatus)
-                .setMessage("Score is " + score + " out of " + totalQuestions)
-                .setPositiveButton("Restart", ((dialogInterface, i) -> restartQuiz()))
-                .setCancelable(false)
-                .show();
-    }
-    private void restartQuiz() {
-        score = 0;
-        currentQuestionIndex = 0;
-        selectedAnswer = "";
-        okButton.setEnabled(true);
-        loadNewQuestion();
-    }
 }
--- app/src/main/java/com/example/myapplicationnnnnnn/QuestionAnswers.java
+++ app/src/main/java/com/example/myapplicationnnnnnn/QuestionAnswers.java
@@ -1,22 +1,56 @@
 package com.example.myapplicationnnnnnn;
 
-public class QuestionAnswers {
-    public static String question[] = {
-            "what note does an octave start on?",
-            "How much is 2 + 2 ?",
-            "How much is 3 + 3 ?"
-    };
+import android.os.Bundle;
 
-    public static String choices[][] = {
-            { "C", "E", "F", "D" },
-            { "1", "2", "3", "4" },
-            { "1", "2", "3", "6" }
-    };
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-        public static String answers[] = {
-                "C",
-                "4",
-                "6"
-        };
+import java.util.ArrayList;
+
+public class QuestionAnswers extends AppCompatActivity {
+
+    private ArrayList<String> audioNames;
+
+//    public static String question[] = {
+//            "what note does an octave start on?",
+//            "How much is 2 + 2 ?",
+//            "How much is 3 + 3 ?"
+//    };
+//
+//    public static String choices[][] = {
+//            { "C", "E", "F", "D" },
+//            { "1", "2", "3", "4" },
+//            { "1", "2", "3", "6" }
+//    };
+//
+//        public static String answers[] = {
+//                "C",
+//                "4",
+//                "6"
+//        };
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        audioNames = new ArrayList<>();
+
+        audioNames.add("prima.mp3");
+        audioNames.add("poqr_sekunda.mp3");
+        audioNames.add("mec_sekunda.mp3");
+        audioNames.add("poqr_tercia.mp3");
+        audioNames.add("mec_tercia.mp3");
+        audioNames.add("maqur_kvarta.mp3");
+        audioNames.add("maqur_kvinta.mp3");
+        audioNames.add("poqr_seksta.mp3");
+        audioNames.add("mec_seksta.mp3");
+        audioNames.add("poqr_septima.mp3");
+        audioNames.add("mec_septima.mp3");
+        audioNames.add("oktava.mp3");
+    }
+
+
 }
 
